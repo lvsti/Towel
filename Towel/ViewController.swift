8da58a0f335f287ad9c905b480a594b87b9e150b
Tamas Lustyik <elveestei@gmail.com>
2016-04-09 19:46:30 +0200

    Simple hitchhiking place visualization

--- Towel/ViewController.swift
+++ Towel/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+extension Place: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2DMake(latitude, longitude)
+    }
+}
 
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    private var _isModifyingMap: Bool = false
+    private let _dataModel: DataModel
+    private lazy var _places: [Place] = {
+        return try! self._dataModel.getAllPlaces()
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        _dataModel = DataModel.instance
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        return MKTileOverlayRenderer(overlay: overlay)
     }
-
-
+    
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        // show/hide annotations based on zoom level
+        if mapView.region.span.latitudeDelta < 2 {
+            mapView.addAnnotations(_places)
+        } else {
+            mapView.removeAnnotations(_places)
+        }
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if let place = annotation as? Place {
+            let pin = PlacePin(annotation: place, reuseIdentifier: "place")
+            pin.configure()
+            return pin
+        }
+        
+        return nil
+    }
+    
 }
 
