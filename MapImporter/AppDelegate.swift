9648740da656d146b4d536051b67ebf11e608262
Tamas Lustyik <elveestei@gmail.com>
2016-04-10 21:14:04 +0200

    Created OSX map importer app skeleton

---
+++ MapImporter/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  MapImporter
+//
+//  Created by Tamas Lustyik on 2016. 04. 10..
+//  Copyright © 2016. Tamas Lustyik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+    
+
+}
+
