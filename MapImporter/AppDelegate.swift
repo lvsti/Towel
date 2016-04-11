ada16b69c6379d71dc1ab3ed8920d9c92ced020e
Tamas Lustyik <elveestei@gmail.com>
2016-04-11 23:26:45 +0200

    Implemented Realm importer

--- MapImporter/AppDelegate.swift
+++ MapImporter/AppDelegate.swift
@@ -7,22 +7,164 @@
 //
 
 import Cocoa
+import RealmSwift
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    let fileManager: NSFileManager
+    
+    override init() {
+        fileManager = NSFileManager.defaultManager()
+        super.init()
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        dispatch_async(dispatch_get_main_queue(), importPlaces)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    func importPlaces() {
+        let placesDirURL = NSURL(fileURLWithPath: "/Users/lvsti/tw/places/")
+        let placeFiles = try! fileManager.contentsOfDirectoryAtPath(placesDirURL.path!)
+        
+        let lastImportedID = "\(Query.getLastImportedPlaceInfo()?.placeID ?? 0)"
+        
+        var counter = 0
+        let realm = try! Realm()
+        realm.beginWrite()
+        
+        (placeFiles as [String]).forEach { filePath in
+            if (filePath as NSString).pathExtension != "json" ||
+                ((filePath as NSString).lastPathComponent as NSString).stringByDeletingPathExtension <= lastImportedID {
+                return
+            }
+            
+            let placeData = NSData(contentsOfURL: placesDirURL.URLByAppendingPathComponent(filePath))
+            let place: NSDictionary = try! NSJSONSerialization.JSONObjectWithData(placeData!, options: []) as! NSDictionary
+            NSLog("processing %@", (filePath as NSString).lastPathComponent)
+            let dbPlace = processPlace(place)
+            realm.add(dbPlace)
+            
+            counter += 1
+            if counter == 10 {
+                try! realm.commitWrite()
+                counter = 0
+                realm.beginWrite()
+            }
+        }
+        try! realm.commitWrite()
+    }
     
+    func processPlace(json: NSDictionary) -> DBPlace {
+        let place = DBPlace()
+        place._latitude = json["lat"]!.doubleValue
+        place._longitude = json["lon"]!.doubleValue
+        place._avgRating = json["rating"]!.floatValue
+        if let stats = json["waiting_stats"] as? NSDictionary,
+           let avg = stats["avg"] as? NSNumber {
+            place._avgWaiting.value = avg.floatValue
+        }
 
+        let info = DBPlaceInfo()
+        info._placeID = Int32(json["id"] as! String)!
+        if let alt = json["elevation"]?.doubleValue {
+            info._altitude.value = alt
+        }
+        
+        if let comments = json["comments"] as? [NSDictionary] where comments.count > 0 {
+            comments.forEach { comment in
+                let placeComment = DBPlaceComment()
+                placeComment._commentID = Int32(comment["id"] as! String)!
+                placeComment._text = comment["comment"] as! String
+                if let datetime = comment["datetime"] as? String {
+                    placeComment._timestamp = parseDate(datetime)!
+                }
+                placeComment._user = processUser(comment["user"] as? NSDictionary)
+                
+                info._comments.append(placeComment)
+            }
+        }
+        
+        info._location = processLocation(json["location"] as? NSDictionary)
+        
+        if let descriptions = json["description"] as? [String: AnyObject] where descriptions.count > 0 {
+            descriptions.forEach { (langID: String, anyDesc: AnyObject) in
+                let desc = anyDesc as! NSDictionary
+                let placeDesc = DBPlaceDescription()
+                placeDesc._languageID = langID
+                placeDesc._text = desc["description"] as! String
+                if let datetime = desc["datetime"] as? String {
+                    placeDesc._timestamp = parseDate(datetime)!
+                }
+                if let user = desc["fk_user"] as? String {
+                    placeDesc._user = Query.getUserByID(Int32(user)!)
+                }
+                
+                info._descriptions.append(placeDesc)
+            }
+        }
+        
+        info._user = processUser(json["user"] as? NSDictionary)
+
+        place._placeInfo = info
+        
+        return place
+    }
+    
+    func parseDate(str: String) -> NSDate? {
+        return dateFormatter.dateFromString(str)
+    }
+    
+    func processUser(json: NSDictionary?) -> DBUser? {
+        guard
+            let json = json,
+            let userIDStr = json["id"] as? String
+        else {
+            return nil
+        }
+        
+        let userID = Int32(userIDStr)!
+        if let user = Query.getUserByID(userID) {
+            return user
+        }
+        
+        let user = DBUser()
+        user._userID = userID
+        user._username = (json["name"] as! String)
+        return user
+    }
+    
+    func processLocation(json: NSDictionary?) -> DBLocation? {
+        guard let json = json else {
+            return nil
+        }
+        
+        let locality = json["locality"] as? String
+        let countryID = (json["country"] as! NSDictionary)["iso"] as! String
+        if let location = Query.getLocationForLocality(locality, inCountry: countryID) {
+            return location
+        }
+        
+        let location = DBLocation()
+        location._locality = locality
+        location._countryID = countryID
+        location._continentID = (json["continent"] as! NSDictionary)["code"] as! String
+        
+        return location
+    }
+
+    lazy var dateFormatter: NSDateFormatter = {
+        let fmt = NSDateFormatter()
+        fmt.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return fmt
+    }()
+    
 }
 
---
+++ MapImporter/Query.swift
@@ -0,0 +1,40 @@
+//
+//  Query.swift
+//  Towel
+//
+//  Created by Tamas Lustyik on 2016. 04. 10..
+//  Copyright © 2016. Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Query {
+    
+    static func getLastImportedPlaceInfo() -> DBPlaceInfo? {
+        let realm = try! Realm()
+        return realm.objects(DBPlaceInfo).sorted("_placeID", ascending: false).first
+    }
+    
+    static func getUserByID(id: Int32) -> DBUser? {
+        let realm = try! Realm()
+        return realm.objects(DBUser).filter("_userID == %d", NSNumber(int: id)).first
+    }
+    
+    static func getLocationForLocality(locality: String?, inCountry countryID: String) -> DBLocation? {
+        let realm = try! Realm()
+        var predicate: NSPredicate!
+        
+        if let locality = locality {
+            predicate = NSPredicate(format: "_locality == %@ AND _countryID == %@",
+                locality,
+                countryID)
+        } else {
+            predicate = NSPredicate(format: "_locality == NULL AND _countryID == %@",
+                countryID)
+        }
+        
+        return realm.objects(DBLocation).filter(predicate).first
+    }
+
+}
